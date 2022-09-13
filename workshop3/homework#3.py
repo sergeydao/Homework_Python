2421d192324ee15d64a929962a1149aa1bdf5c6a
Sergey Dolgunov <dolgunov.s@gmail.com>
2022-09-13 22:15:30 +0300

    add task# 2

--- workshop3/homework#3.py
+++ workshop3/homework#3.py
@@ -4,9 +4,26 @@
 
 # Пример: [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
 
-list = [2, 3, 5, 9, 3,]
-sum = 0
-for i in range(len(list)):
-    if i % 2 == 1:
-        sum += list[i]
-print(sum)
+# list = [2, 3, 5, 9, 3,]
+# sum = 0
+# for i in range(len(list)):
+#     if i % 2 == 1:
+#         sum += list[i]
+# print(sum)
+
+# 2. Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+# Пример:
+# [2, 3, 4, 5, 6] => [12, 15, 16];
+# [2, 3, 5, 6] => [12, 15]
+
+list = [2, 3, 4, 5, 6]
+result = []
+maxIndex = len(list) - 1
+
+for i in range(len(list) // 2):
+    result.append(list[i] * list[maxIndex])
+    maxIndex -= 1
+if len(list) % 2 == 1:
+    result.append(list[len(list) // 2]**2)
+print(result)
